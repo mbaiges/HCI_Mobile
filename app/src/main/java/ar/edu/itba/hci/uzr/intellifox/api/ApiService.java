7e62ca484d72cb76b1c136bc60a5f912c58b3ed3
AndyBilevich <abilevich@itba.edu.ar>
2020-06-18 13:08:02 -0300

    added api

---
+++ app/src/main/java/ar/edu/itba/hci/uzr/intellifox/api/ApiService.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.hci.uzr.intellifox.api;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface ApiService {
+
+    @POST("rooms")
+    @Headers("Content-Type: application/json")
+    Call<Result<Room>> addRoom(@Body Room room);
+
+    @PUT("rooms/{roomId}")
+    @Headers("Content-Type: application/json")
+    Call<Result<Boolean>> modifyRoom(@Path("roomId") String roomId, @Body Room room);
+
+    @DELETE("rooms/{roomId}")
+    Call<Result<Boolean>> deleteRoom(@Path("roomId") String roomId);
+
+    @GET("rooms/{roomId}")
+    Call<Result<Room>> getRoom(@Path("roomId") String roomId);
+
+    @GET("rooms")
+    Call<Result<List<Room>>> getRooms();
+}
