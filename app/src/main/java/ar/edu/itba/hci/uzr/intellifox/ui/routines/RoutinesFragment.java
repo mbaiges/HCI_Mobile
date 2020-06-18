cff32ab6e71da7c188e2ba3d02376201ed10f287
Gabriel Margossian <gmargossian@itba.edu.ar>
2020-06-18 13:02:55 -0300

    working

--- app/src/main/java/ar/edu/itba/hci/uzr/intellifox/ui/routines/RoutinesFragment.java
+++ app/src/main/java/ar/edu/itba/hci/uzr/intellifox/ui/routines/RoutinesFragment.java
@@ -19,40 +19,12 @@ import ar.edu.itba.hci.uzr.intellifox.R;
 public class RoutinesFragment extends Fragment {
 
     ListView listView;
-    ConstraintLayout expandableView;
-    Button arrowBtn;
-    CardView cardView;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        getActivity().setContentView(R.layout.device_card_item);
         View root = inflater.inflate(R.layout.fragment_routines, container, false);
         listView = root.findViewById(R.id.routines_list_view);
-
-        // ---------------------------------------------------------
-
-        expandableView = getActivity().findViewById(R.id.expandableView);
-        arrowBtn = getActivity().findViewById(R.id.arrowBtn);
-        cardView = getActivity().findViewById(R.id.cardView);
-
-        arrowBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (expandableView.getVisibility()==View.GONE){
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.VISIBLE);
-                    arrowBtn.setBackgroundResource(R.drawable.ic_chevron_up);
-                } else {
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.GONE);
-                    arrowBtn.setBackgroundResource(R.drawable.ic_chevron_down);
-                }
-            }
-        });
-
-
-        // ----------------------------------------------------------
 
         Routine[] values = new Routine[]{
                 new Routine(1, "Open all doors", "#FFFFFF"),
@@ -61,7 +33,7 @@ public class RoutinesFragment extends Fragment {
                 new Routine(4, "Play cumbia in all speakers", "#EAEAEA"),
                 new Routine(5, "Drop all the water", "#667788")
         };
-        RoutineArrayAdapter adapter = new RoutineArrayAdapter(this.getActivity(), values);
+        DeviceArrayAdapter adapter = new DeviceArrayAdapter(this.getActivity(), values);
 
         listView.setAdapter(adapter);
         return root;
