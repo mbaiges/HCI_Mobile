4a26eeab1eeef79e2ea8b21d6526e60b43834438
CamilaMarianaPonce <cponce@itba.edu.ar>
2020-06-23 14:22:39 -0300

    tried rooms

--- app/src/main/java/ar/edu/itba/hci/uzr/intellifox/ui/devices/vacuum/VacuumDeviceObserver.java
+++ app/src/main/java/ar/edu/itba/hci/uzr/intellifox/ui/devices/vacuum/VacuumDeviceObserver.java
@@ -1,12 +1,24 @@
 package ar.edu.itba.hci.uzr.intellifox.ui.devices.vacuum;
 
+import android.app.Activity;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import ar.edu.itba.hci.uzr.intellifox.R;
 import ar.edu.itba.hci.uzr.intellifox.api.ApiClient;
@@ -19,9 +31,12 @@ import ar.edu.itba.hci.uzr.intellifox.api.models.devices.DoorDevice;
 import ar.edu.itba.hci.uzr.intellifox.api.models.devices.DoorDeviceState;
 import ar.edu.itba.hci.uzr.intellifox.api.models.devices.VacuumDevice;
 import ar.edu.itba.hci.uzr.intellifox.api.models.devices.VacuumDeviceState;
+import ar.edu.itba.hci.uzr.intellifox.api.models.room.Room;
+import ar.edu.itba.hci.uzr.intellifox.api.models.room.RoomArrayAdapter;
 import ar.edu.itba.hci.uzr.intellifox.ui.devices.DeviceObserver;
 
 import ar.edu.itba.hci.uzr.intellifox.ui.devices.ac.ACDeviceViewHolder;
+import ar.edu.itba.hci.uzr.intellifox.ui.rooms.RoomsViewModel;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,7 +44,7 @@ import retrofit2.Response;
 public class VacuumDeviceObserver extends DeviceObserver {
 
 
-    private static final String DOCK= "dock";
+    private static final String DOCK = "dock";
 
     public VacuumDeviceObserver(View contextView) {
         super(contextView);
@@ -61,13 +76,11 @@ public class VacuumDeviceObserver extends DeviceObserver {
             VacuumDeviceViewHolder h = (VacuumDeviceViewHolder) holder;
 
             String status = s.getStatus();
-            if(status.equals("active")){
+            if (status.equals("active")) {
                 status = contextView.getResources().getString(R.string.dev_vacuum_state_active);
-            }
-            else if(status.equals("inactive")){
+            } else if (status.equals("inactive")) {
                 status = contextView.getResources().getString(R.string.dev_vacuum_state_inactive);
-            }
-            else if(status.equals("docked")) {
+            } else if (status.equals("docked")) {
                 status = contextView.getResources().getString(R.string.dev_vacuum_state_docked);
             }
 
@@ -75,21 +88,19 @@ public class VacuumDeviceObserver extends DeviceObserver {
             String bat = s.getBatteryLevel().toString();
 
             String mode = s.getMode();
-            if(mode.equals("mop")){
+            if (mode.equals("mop")) {
                 mode = contextView.getResources().getString(R.string.dev_vacuum_button_mop);
-            }
-            else if (mode.equals("vacuum")){
+            } else if (mode.equals("vacuum")) {
                 mode = contextView.getResources().getString(R.string.dev_vacuum_button_vacuum);
             }
 
 
-            if (status != null && bat != null && mode != null ) {
+            if (status != null && bat != null && mode != null) {
                 String aux;
-                if(h.icon != null) {  //si esta el icono
+                if (h.icon != null) {  //si esta el icono
                     aux = status;
-                }
-                else
-                    aux = status + "-" + mode + "-" + contextView.getResources().getString(R.string.dev_vacuum_battery) + ": " + bat + "%" ;
+                } else
+                    aux = status + "-" + mode + "-" + contextView.getResources().getString(R.string.dev_vacuum_battery) + ": " + bat + "%";
                 if (h.description != null) {
                     h.description.setText(aux);
                 }
@@ -122,34 +133,77 @@ public class VacuumDeviceObserver extends DeviceObserver {
                 h.dockBtn.setText((s.getStatus().equals("docked")) ? R.string.dev_vacuum_button_docked : R.string.dev_vacuum_button_dock);
             }
         }
+
+        ApiClient.getInstance().getRooms(new Callback<Result<List<Room>>>() {
+            @Override
+            public void onResponse(@NonNull Call<Result<List<Room>>> call, @NonNull Response<Result<List<Room>>> response) {
+                MutableLiveData<Object> mRooms = new MutableLiveData<>();
+                if (response.isSuccessful()) {
+                    Result<List<Room>> result = response.body();
+                    if (result != null) {
+                        Set<Room> actualRoomsSet = new HashSet<>(result.getResult());
+                        Set<Room> roomsSet = (Set<Room>) mRooms.getValue();
+
+                        // Spinner element
+                        Spinner spinner = contextView.findViewById(R.id.rooms_spinner);
+                        
+                        // Creating adapter for spinner
+                        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, roomsSet);
+
+                        // Drop down layout style - list view with radio button
+                        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                        // attaching data adapter to spinner
+                        spinner.setAdapter(dataAdapter);
+
+//                        if (roomsSet == null || !(roomsSet.equals(actualRoomsSet))) {
+//                            mRooms.postValue(actualRoomsSet);
+//                            VacuumDeviceViewHolder h = (VacuumDeviceViewHolder) holder;
+//                            for (Room r : actualRoomsSet) {
+//                                contextView.findViewById(R.id.rooms_spinner).
+//                            }
+//                        }
+
+                    } else {
+                        handleError(response);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Result<List<Room>>> call, @NonNull Throwable t) {
+                handleUnexpectedError(t);
+            }
+        });
     }
+
 
     @Override
     protected void attachFunctions() {
         super.attachFunctions();
         VacuumDeviceViewHolder h = (VacuumDeviceViewHolder) holder;
 
-        if(h.modeBtn != null){
-            for(int i = 0 ; i < 2 ; i++){
-                if(h.modeBtn[i] != null){
-                    Pair<ToggleButton,String> aux = (Pair<ToggleButton, String>) h.modeBtn[i];
-                    if(aux.first != null){
+        if (h.modeBtn != null) {
+            for (int i = 0; i < 2; i++) {
+                if (h.modeBtn[i] != null) {
+                    Pair<ToggleButton, String> aux = (Pair<ToggleButton, String>) h.modeBtn[i];
+                    if (aux.first != null) {
                         aux.first.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 VacuumDevice d = (VacuumDevice) h.device;
-                                if(d != null){
+                                if (d != null) {
                                     VacuumDeviceState s = d.getState();
-                                    if(s != null){
+                                    if (s != null) {
                                         String[] args = {aux.second};
                                         ApiClient.getInstance().executeDeviceAction(d.getId(), "setMode", args, new Callback<Result<Object>>() {
                                             @Override
                                             public void onResponse(Call<Result<Object>> call, Response<Result<Object>> response) {
-                                                if(response.isSuccessful()){
+                                                if (response.isSuccessful()) {
                                                     Result<Object> result = response.body();
-                                                    if(result != null){
-                                                        Object success =  result.getResult();
-                                                        if(success != null){
+                                                    if (result != null) {
+                                                        Object success = result.getResult();
+                                                        if (success != null) {
                                                             Log.v("ACTION_SUCCESS", success.toString());
                                                             clearModeSelections();
                                                             aux.first.setChecked(true);
@@ -157,6 +211,7 @@ public class VacuumDeviceObserver extends DeviceObserver {
                                                     }
                                                 }
                                             }
+
                                             @Override
                                             public void onFailure(Call<Result<Object>> call, Throwable t) {
                                                 handleUnexpectedError(t);
@@ -211,25 +266,22 @@ public class VacuumDeviceObserver extends DeviceObserver {
     }
 
 
-
-
     @Override
     protected String getOnSwitchActionName(Boolean switchStatus) {
-        return switchStatus?"start":"pause";
+        return switchStatus ? "start" : "pause";
     }
 
-    private void clearModeSelections(){
+    private void clearModeSelections() {
         VacuumDeviceViewHolder h = (VacuumDeviceViewHolder) holder;
-        for (int i = 0 ; i<h.modeBtn.length ; i++){
-            if(h.modeBtn[i] != null){
-                Pair<ToggleButton,String> aux = (Pair<ToggleButton, String>) h.modeBtn[i];
-                if(aux.first != null){
+        for (int i = 0; i < h.modeBtn.length; i++) {
+            if (h.modeBtn[i] != null) {
+                Pair<ToggleButton, String> aux = (Pair<ToggleButton, String>) h.modeBtn[i];
+                if (aux.first != null) {
                     aux.first.setChecked(false);
                 }
 
             }
         }
     }
-
 
 }
