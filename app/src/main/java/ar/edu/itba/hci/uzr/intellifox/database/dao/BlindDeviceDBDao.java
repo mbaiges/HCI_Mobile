1ab74a07668af09699a129421aa5c32c926f9fa2
CamilaMarianaPonce <cponce@itba.edu.ar>
2020-06-23 23:48:06 -0300

    daos

---
+++ app/src/main/java/ar/edu/itba/hci/uzr/intellifox/database/dao/BlindDeviceDBDao.java
@@ -0,0 +1,4 @@
+package ar.edu.itba.hci.uzr.intellifox.database.dao;
+
+public interface BlindDeviceDBDao {
+}
---
+++ app/src/main/java/ar/edu/itba/hci/uzr/intellifox/database/models/BlindDeviceDB.java
@@ -0,0 +1,4 @@
+package ar.edu.itba.hci.uzr.intellifox.database.models;
+
+public class BlindDeviceDB {
+}
